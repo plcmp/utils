42aed833cc5dc5495c908c1256fba3e8d3676d8c
Azat Yamalov <yamalov.azat@live.com>
2022-02-22 11:12:42 +0300

    Import fixed for registry sensitive OS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { debounce } from "./debounce.js";
 import { throttle } from "./throttle.js";
 import ControlledArray from "./ControlledArray.js";
-import PlaceHolder from "./Placeholder.js";
+import PlaceHolder from "./PlaceHolder.js";
 import { addOverlay, removeOverlay } from "./OverlayManager.js";
 import PlResizeableMixin from "./ResizeableMixin.js";
 
